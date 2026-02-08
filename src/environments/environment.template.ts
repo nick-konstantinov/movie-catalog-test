6ef2da94d4d7b1c83ec94ea43e8f92af0fe95093
Nick Konstantinov <nick.konstantinov.job@gmail.com>
2026-02-08 23:06:23 +0400

    chore: add environment template

---
+++ src/environments/environment.template.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  tmdbApiKey: '__API_KEY__',
+  tmdbApiUrl: 'https://api.themoviedb.org/3',
+  tmdbImageUrl: 'https://image.tmdb.org/t/p/w500',
+  tmdbLanguage: 'ru-RU'
+};
