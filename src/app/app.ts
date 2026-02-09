54a32b7e21f7dd6864b8fff90acf5d6805f1a595
Nick Konstantinov <nick.konstantinov.job@gmail.com>
2026-02-09 22:38:21 +0400

    feat: add footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './layout/header/header';
 import {MovieList} from './features/movies/components/movie-list/movie-list';
+import {Footer} from './layout/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, MovieList],
+  imports: [RouterOutlet, Header, MovieList, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
